4e7a4d8033db5c5f04ce8c8e39b0fe48d8acbaf9
Beatriz Rizental <beatriz.rizental@gmail.com>
2018-10-31 09:37:19 -0300

    exercicio 02-dom resolvido

--- 02-dom/main.js
+++ 02-dom/main.js
@@ -1,4 +1,5 @@
 const input_cpf = document.getElementById("cpf")
+
 input_cpf.addEventListener("focus" , function(event) {
     input_cpf.value = "___.___.___-__"
     setTimeout(function() {
@@ -23,6 +24,21 @@ input_cpf.addEventListener("focus" , function(event) {
     // set_cursor_position = set_cursor_position.bind(this)
     // setTimeout(set_cursor_position, 1)
 })
+
 input_cpf.addEventListener("blur" , function() {
     this.value = ""
+})
+
+input_cpf.addEventListener("keydown", function(event) {
+    event.preventDefault()
+    if("0123456789".indexOf(event.key) !== -1
+        && this.value.indexOf("_") !== -1) {
+            this.value = this.value.replace(/_/, event.key)
+            const next_index = this.value.indexOf("_")
+            this.setSelectionRange(next_index, next_index)
+    } else if (event.key === "Backspace") {
+        this.value = this.value.replace(/(\d$)|(\d(?=\D+$))/, "_")
+        const next_index = this.value.indexOf("_")
+        this.setSelectionRange(next_index, next_index)
+    }
 })
